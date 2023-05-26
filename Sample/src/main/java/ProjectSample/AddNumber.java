c1e57cfff379bf4dc4a1751617d55ce697400015
DELL <DELL@DESKTOP-TT2Q8LS>
2023-05-26 16:02:49 +0530

    commit

---
+++ Sample/src/main/java/ProjectSample/AddNumber.java
@@ -0,0 +1,11 @@
+package ProjectSample;
+
+public class AddNumber {
+
+	public static void main(String[] args) {
+		int a=10,b=20,c;
+		c=a+b;
+		System.out.println("Sum of two  number =" +c);
+	}
+
+}
